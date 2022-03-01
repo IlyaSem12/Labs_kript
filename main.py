d6144c6add8956fb08eb50bc23541c93a0faa19c
IlyaSem12 <91151136+IlyaSem12@users.noreply.github.com>
2022-03-01 23:22:35 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,31 +2,60 @@ from tkinter import *
 
 window = Tk()
 window.title("Шифр цезаря")
-window.geometry('600x500')
+window.resizable(False,False)
 
 alf = 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ'
 
 
+def cripto():
+    word = entr_txt.get().upper()
+    step = int(entrstep.get())
 
-word = input('Введите слово:').upper()
-step = int(input('Введите шаг:'))
+    message = ""
+    revmessage = ""
 
-revmessage = ""
+    for i in word:
+        place = alf.find(i)
+        new_place = place + step
+        if i in alf:
+            message += alf[new_place]
+        else:
+            message += i
 
-for i in word:
-    place = alf.find(i)
-    new_place = place + step
-    if i in alf:
-        revmessage += alf[new_place]
-    else:
-        revmessage += i
+    for i in message:
+        place = alf.find(i)
+        new_place = place - step
+        if i in alf:
+            revmessage += alf[new_place]
+        else:
+            revmessage += i
+
+    print(message)
+    print(revmessage)
+    text.insert(1.0, message + '\n')
+    text.insert(1.0, revmessage + '\n')
 
 
-print(revmessage)
+lbl= Label(window,text="Введите слово:").grid(column=0,row=0,pady=8, padx=8)
+lbl2= Label(window,text="Введите шаг:").grid(column=0,row=1,pady=8, padx=8,sticky=W)
 
 
-labl1 = Label(window,text="Введите слово:")
-labl1.grid(row=2,column=0)
+entr_txt=Entry(window,width=30)
+entr_txt.grid(column=1,row=0,columnspan=3,pady=8, padx=8)
+entrstep=Entry(window,width=15)
+entrstep.grid(column=1,row=1,pady=8, padx=8,sticky=W)
+
+
+btn_go = Button(window,text="Пуск",command=cripto).grid(column=2,row=1,pady=8, padx=8,sticky=W)
+
+
+frame=LabelFrame(text='Вывод')
+frame.grid(column=4,row=0,pady=8, padx=8,rowspan=2,columnspan=2)
+
+text=Text(frame,width=30, height=5)
+text.pack(side=LEFT)
+
+
 
 
 window.mainloop()
