27fcc6f9b04a618defff28d062f9c92619adb8f4
IlyaSem12 <91151136+IlyaSem12@users.noreply.github.com>
2022-03-13 20:58:18 +0400

    Add files via upload

---
+++ Perestav.py
@@ -0,0 +1,156 @@
+from tkinter import *
+from tkinter import messagebox as mb
+window = Tk()
+window.title("Шифр перестановки")
+window.resizable(False,False)
+
+
+def cripto(key, message):
+    encodemas = []
+    tabl = []
+    cipher = {}
+    result = ' '
+    count = 0
+    a = message.split()
+    messagetxt = ''.join(a).lower()
+
+    if len(key) > len(messagetxt):
+        for i in range(len(key) * 2 - len(messagetxt)):
+            messagetxt += '*'
+
+    if len(messagetxt) % len(key) != 0:
+        while len(messagetxt) % len(key) != 0:
+            messagetxt += '*'
+
+    stroki = len(messagetxt) // len(key)
+
+    for index, ch in enumerate(key.lower()):
+        if ch in cipher:
+            cipher[ch] += messagetxt[index * stroki: index * stroki + stroki]
+        else:
+            cipher[ch] = messagetxt[index * stroki: index * stroki + stroki]
+
+    for index in range(stroki):
+        for i in sorted(key.lower()):
+            encodemas.append([cipher[i][index]])
+            count += 1
+            if count == stroki:
+                encodemas.append(" ")
+                count = 0
+
+    for i in range(len(encodemas)):
+        result += str(*encodemas[i])
+    result = result.split()
+
+    for i in range(stroki):
+        tabl.append([])
+        tabl[i] += messagetxt[i * len(key): i * len(key) + len(key)]
+
+    for i in range(len(tabl)):
+        print(tabl[i])
+
+    for i in range(len(result)):
+        # messagetxt += message[i]
+        txt = ''.join(result).lower()
+
+    text.insert(1.0, txt  + '\n')
+
+    return result
+
+
+#######################################################
+
+def decripto(key, message):
+    encodemas = []
+    decodemas = []
+    cipher = {}
+    result = ' '
+    count = 0
+    a = message.split()
+    messagetxt = ''.join(a).lower()  # убираем пробелы
+
+
+    sortkey = sorted(key.lower())
+    stroki = len(messagetxt) // len(key)
+
+    print("message", message)
+
+    print("messagetxt", messagetxt)
+
+    for i in range(stroki):
+        decodemas.append([])
+        decodemas[i] += messagetxt[i * len(key): i * len(key) + len(key)]
+
+    for i in range(len(decodemas)):
+        print(decodemas[i])
+
+    destr = ""
+
+    for i in range(len(key)):
+        for j in range(stroki):
+            destr += decodemas[j][i]
+
+    print("destr", destr)
+
+    for index, ch in enumerate(sortkey):
+        if ch in cipher:
+            cipher[ch] += destr[index * stroki: index * stroki + stroki]
+        else:
+            cipher[ch] = destr[index * stroki: index * stroki + stroki]
+
+    for i in key.lower():
+        encodemas.append([cipher[i]])
+        count += 1
+
+    for i in range(len(encodemas)):
+        result += str(*encodemas[i])
+    result = result.split()
+
+    for i in range(len(result)):
+        # messagetxt += message[i]
+        txt = ''.join(result).lower()
+
+    text.insert(1.0, txt + '\n')
+
+    return result
+
+##############кнопки#########################################################
+
+lbl= Label(window,text="Введите текст:").grid(column=0,row=0,pady=8, padx=8)
+lbl2= Label(window,text="Введите ключ:").grid(column=0,row=1,pady=8, padx=8,sticky=W)
+
+
+entr_txt=Entry(window,width=30)
+entr_txt.grid(column=1,row=0,columnspan=3,pady=8, padx=8)
+entrkey=Entry(window,width=15)
+entrkey.grid(column=1,row=1,pady=8, padx=8,sticky=W)
+
+btn_go = Button(window, text="жмак")
+btn_go.grid(column=2, row=1, pady=8, padx=8, sticky=W)
+
+
+r_var = IntVar()
+r_var.set(0)
+r1 = Radiobutton(text='Зашифровать',
+                 variable=r_var, value=0,)
+
+r2 = Radiobutton(text='Расшифровать',
+                 variable=r_var, value=1)
+
+r1.grid(column=4,row=0,pady=8, padx=8)
+r2.grid(column=4,row=1,pady=8, padx=8)
+
+
+if r_var.get() == 0:
+    btn_go.config(command=lambda: cripto(entrkey.get(), entr_txt.get()))
+elif r_var.get() == 1:
+    print('хуйня')
+    btn_go.config(command=lambda: decripto(entrkey.get(), entr_txt.get()))
+
+frame=LabelFrame(text='Вывод')
+frame.grid(column=0,row=2,pady=8, padx=8,rowspan=2,columnspan=2)
+
+text=Text(frame,width=30, height=5)
+text.pack(side=LEFT)
+
+window.mainloop()
